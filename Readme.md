dbfcdece52d6e3624ea6bc053f9f8c42516b0250
Soufiane <68408817+Jarvis-codergit@users.noreply.github.com>
2022-02-17 23:14:48 +0000

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is a GPT-3 webiste & one of my personal projects 
+
+======> AI Is The Future
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
+
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">GPT-3 | JarvessAI</h1>
+      <p>GPT-3 In short, means that it generates text using algorithms that are pre-trained. Already been fed all of the data they need to carry out their task. Specifically, they’ve been fed around 570gb of text information gathered by crawling the internet ( A publicly available dataset known as CommonCrawl ) along with other texts selected by OpenAI, including the text of Wikipedia</p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
+
+export default Header;
